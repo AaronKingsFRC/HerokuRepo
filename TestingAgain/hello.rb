02878d845986885f1e1af1c0c4d90942d772f227
AaronBae <abae777@gmail.com>
2014-11-10 16:26:03 -0800

    First Initialization

---
+++ TestingAgain/hello.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+requira 'sinatra/base'
+require 'json_pure'
+
+class HelloService <Sinatra::Base
+    before do
+      content_type :json
+    end
+
+  get '/' do
+    {:msg => "Hello World"}.to_json
+  end
+end
